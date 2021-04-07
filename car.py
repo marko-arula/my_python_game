3a52fae7196ae1097fba72a5d7147ed0e24e7ee6
Marko Arula <marko.arula@khk.ee>
2021-04-07 21:01:14 +0300

    Most recent commit

--- car.py
+++ car.py
@@ -10,16 +10,22 @@ class Car():
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.center = float(self.rect.centerx)
-        self.rect.bottom = self.screen_rect.bottom
+        self.centery = float(self.rect.centery)
+        self.rect.center = self.screen_rect.center
         self.moving_right = False
         self.moving_left = False
-
+        self.moving_up = False
+        self.moving_down = False
     def update(self):
         """Update car position according to moving flag"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.game_settings.car_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.game_settings.car_speed_factor
+        if self.moving_up:
+            self.centery += self.game_settings.car_speed_factor
+        if self.moving_down:
+            self.centery -= self.game_settings.car_speed_factor
         self.rect.centerx = self.center
     def blitme(self):
         """Draw car at this position"""
--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,17 @@
 import sys
 import pygame
+def check_keydown_events(event, car):
+    """Check your keyboard events"""
+    if event.key == pygame.K_RIGHT:
+        car.moving_right = True
+    if event.key == pygame.K_LEFT:
+        car.moving_left = True
+def check_keyup_events(event, car):
+    """Check your keyboard events"""
+    if event.key == pygame.K_RIGHT:
+        car.moving_right = False
+    if event.key == pygame.K_LEFT:
+        car.moving_left = False
 def check_events(car):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -9,11 +21,19 @@ def check_events(car):
                 car.moving_right = True
             if event.key == pygame.K_LEFT:
                 car.moving_left = True
+            if event.key == pygame.K_UP:
+                car.moving_up = True
+            if event.key == pygame.K_DOWN:
+                car.moving_down = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 car.moving_right = False
             if event.key == pygame.K_LEFT:
                 car.moving_left = False
+            if event.key == pygame.K_UP:
+                car.moving_up = False
+            if event.key == pygame.K_DOWN:
+                car.moving_down = False
                 
 def update_screen(game_settings, screen, car):
     # add screen background
