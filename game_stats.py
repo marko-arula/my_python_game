f46bcb669bbe32800bf258e2495976cb34b278cc
Marko Arula <marko.arula@khk.ee>
2021-05-04 11:05:44 +0300

    Made the aliens transparent, made it so the game resets itself after collision

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GameStats():
+    """Game Stats"""
+    def __init__(self, game_settings):
+        """Initialize statistics"""
+        self.game_settings = game_settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics, change during the game"""
+        self.cars_left = self.game_settings.car_limit
